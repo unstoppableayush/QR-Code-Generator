b3cb88d9d2babda4dac9f44bffd8007c44d00d73
unstoppableayush <ayushkum884@gmail.com>
2024-04-02 21:05:25 +0530

    fix:redirection issue

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 function generateQRCode(){
+    event.preventDefault();// fixing redirection issue
     //input value
     let website = document.getElementById("website").value;
     if(website){
@@ -16,10 +18,11 @@ function generateQRCode(){
     }
  }
 
-function downloadQR(){    
-            let downloadLink = document.querySelector(".download");
-            let qrcodeContainer = document.querySelector(".qrcode");
-            downloadLink.href = qrcodeContainer.querySelector('canvas').toDataURL(); // Convert canvas to data URL
-            downloadLink.download = 'qrcode.png'; // Set filename for downloaded image
-            downloadLink.click();
+ function downloadQR(){ 
+    event.preventDefault();// fixing redirection issue   
+    let downloadLink = document.querySelector(".download");
+    let qrcodeContainer = document.querySelector(".qrcode");
+    downloadLink.href = qrcodeContainer.querySelector('canvas').toDataURL(); // Convert canvas to data URL
+    downloadLink.download = 'qrcode.png'; // Set filename for downloaded image
+    downloadLink.click();
 }
